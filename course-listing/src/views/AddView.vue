214d446a5deb5635c7f769d3ed0a6aa88423f2b7
SypnoticVoid <logan.murrie@eagles.oc.edu>
2024-09-19 16:48:09 -0500

    Added View Pages

---
+++ course-listing/src/views/AddView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about">
+    <h1>Add A Course</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
