e6316e63aaaba2e50367fc8e2855810c6d52a588
SypnoticVoid <logan.murrie@eagles.oc.edu>
2024-09-24 09:13:58 -0500

    Added in Course Data Table and added our CSS file to our Index.html

--- course-listing/src/views/AboutView.vue
+++ course-listing/src/views/AboutView.vue
@@ -1,6 +1,56 @@
+
+<script>
+export default 
+{
+  props: {
+    title: String
+  },
+  setup(props) {
+    console.log(props.title)
+  }
+}
+
+Course [];
+
+try
+{
+  fetch(courses);
+
+then
+(
+  data(), 
+  {
+    return:
+    {
+      course: {},
+      group: {},
+      message: "",
+      requestDialog: false,
+      selectedRequest: {
+        CourseNum: "",
+        Department:"",
+        CourseLevel:"",
+        CourseHours:"",
+        CourseName: "",
+        description: "",
+      },
+    },
+  },
+)
+}
+
+catch(error)
+{};
+
+
+
+
+
+</script>
+
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1> Course Listings </h1>
   </div>
 </template>
 
