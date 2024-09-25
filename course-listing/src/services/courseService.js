8d180dcd4497c43e7173647f697b270e5f1d164e
USMCrules02 <cally.piland@eagles.oc.edu>
2024-09-25 15:14:15 -0500

    Added - services folder with the js to conect to the backend.

---
+++ course-listing/src/services/courseService.js
@@ -0,0 +1,16 @@
+import apiClient from "./services.js";
+
+export default {
+    getAllCourses() {
+      return apiClient.get(`/api/courses`);
+    },
+    createCourse(data) {
+      return apiClient.post(`/api/courses`, data);
+    },
+    updateCourse(data) {
+      return apiClient.put(`/api/courses/${id}`, data);
+    },
+    deleteCourse(id) {
+      return apiClient.delete(`/api/courses/${id}`);
+    },
+  };
---
+++ course-listing/src/services/services.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+var baseurl = "http://localhost/"
+
+const apiClient = axios.create({
+  baseURL: baseurl,
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    "X-Requested-With": "XMLHttpRequest",
+    "Access-Control-Allow-Origin": "*",
+    crossDomain: true,
+  }
+});
+export default apiClient;
