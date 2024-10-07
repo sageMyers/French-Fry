ba894f7bcc3a0cfaed4c0b62465e65369c61376a
IanBirdcatcher <ian.fowler@eagles.oc.edu>
2024-10-07 10:13:04 -0500

    changed api calls in courseService.js to course-t2 from /api/courses

--- src/services/courseService.js
+++ src/services/courseService.js
@@ -2,18 +2,18 @@ import apiClient from "./services.js";
 
 export default {
     getAllCourses() {
-      return apiClient.get(`/api/courses`);
+      return apiClient.get(`/course-t2`);
     },
     createCourse(data) {
       console.log("create course");
       console.log(data);
-      return apiClient.post(`/api/courses`, data);
+      return apiClient.post(`/course-t2`, data);
     },
     updateCourse(id,data) {
-      return apiClient.put(`/api/courses/${id}`, data);
+      return apiClient.put(`/course-t2/${id}`, data);
     },
     deleteCourse(id) {
-      return apiClient.delete(`/api/courses/${id}`);
+      return apiClient.delete(`/course-t2/${id}`);
     },
   };
   
